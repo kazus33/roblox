b18b59fff33676062116a0f15eb9894d990634d0
kazus33 <inulek2010@gmail.com>
2026-07-05 03:29:13 +0200

    Fix formatting of SupportedGames entries

--- loader.lua
+++ loader.lua
@@ -1,11 +1,11 @@
 local HubName = "ogfkaza script"
 
 local SupportedGames = {
-    [537413528] = "https://pastefy.app/rYKGsFw2/raw" -- babft
-    [17625359962] = "https://pastefy.app/FmYGBL1t/raw" -- rivals
-    [286090429] = "https://pastefy.app/OLGbabNx/raw" -- arsenal
-    [79546208627805] = "https://pastefy.app/y32IZmen/raw" -- 99nitf
-    [79268393072444] = "https://pastefy.app/Qhb0676T/raw" -- sell lemons
+    [537413528] = "https://pastefy.app/rYKGsFw2/raw", -- babft
+    [17625359962] = "https://pastefy.app/FmYGBL1t/raw", -- rivals
+    [286090429] = "https://pastefy.app/OLGbabNx/raw", -- arsenal
+    [79546208627805] = "https://pastefy.app/y32IZmen/raw", -- 99nitf
+    [79268393072444] = "https://pastefy.app/Qhb0676T/raw", -- sell lemons
     [13822889] = "https://pastefy.app/4DUkq5JK/raw" -- lt2
 --    [] = "" --
 --    [] = "" --
